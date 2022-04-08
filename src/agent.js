1a6900855f30a219776309873cd198b9b3fb87e9
sprtd <sagesdn@gmail.com>
2022-04-08 22:05:29 +0100

    refactor agent

--- src/agent.js
+++ src/agent.js
@@ -1,18 +1,10 @@
-// const { Finding, FindingSeverity, FindingType } = require("forta-agent");
-
 const { Finding, FindingSeverity, FindingType } = require('forta-agent')
 const { GLOBALS } = require('./constants')
-
 
 const { 
   FORTA_CONTRACT, 
   FORTA_DEPLOYER_ADDRESS,
-  DEPLOY_AGENT_EVENT, 
-  FORTA_AGENT_TRANSFER_EVENT, 
-  FORTA_ENABLE_AGENT_FUNCTION, 
   FORTA_CREATE_AGENT_FUNCTION, 
-  TEST_ENABLE_AGENT_DEPLOYER, 
-  TEST_CREATE_AGENT_DEPLOYER
 } = GLOBALS
 
 let findingsCount = 0
@@ -22,21 +14,21 @@ const handleTransaction = txEvent => {
   
   // filter agent for forta deployer's createAgent function triggers
   const fortaAccountDeploymentInvocations = txEvent.filterFunction(FORTA_CREATE_AGENT_FUNCTION, FORTA_CONTRACT)
-  console.log(`forta invocation array: ${fortaAccountDeploymentInvocations}`)
+  // console.log(`forta invocation array: ${fortaAccountDeploymentInvocations}`)
+  // console.log(...fortaAccountDeploymentInvocations)
 
   const { from, to } = txEvent.transaction
 
   console.log(`from: ${from}`)
 
   // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if(!fortaAccountDeploymentInvocations.length || findingsCount >= 5)  {
-    return findings
-  } 
-
+  if(findingsCount >= 5)  return findings
+  
   fortaAccountDeploymentInvocations.forEach(deployment => {
-    console.log(`deployment: ${deployment}`)
-    if(from === FORTA_DEPLOYER_ADDRESS) {
-      findings.push(
+    console.log(`deployment args: ${deployment.args}`)
+    
+    if(from.toLowerCase() === FORTA_DEPLOYER_ADDRESS.toLowerCase()) {
+       findings.push(
         Finding.fromObject({
           name: 'New Agent Deployment',
           description: `New agent deployment by Forta deployer account: ${FORTA_DEPLOYER_ADDRESS}`,
@@ -49,12 +41,12 @@ const handleTransaction = txEvent => {
           }
         })
       )
-      findingsCount ++
-    }
-    return findings
+      findingsCount += findings.length
+    } 
   })
+  console.log(`findings: ${findings}`)
+  return findings
 }
-
 
 module.exports = {
   handleTransaction
--- src/agent.spec.js
+++ src/agent.spec.js
@@ -3,27 +3,29 @@ const {
   FindingSeverity,
   Finding,
   createTransactionEvent,
-
   ethers
-} = require("forta-agent");
+} = require("forta-agent")
+
 const { handleTransaction } = require("./agent");
 
-describe("detect new agent deployments by Forta deployer account",  async () => {
-  describe('', () => {
-    const mockNewAgentDeployFunction = createTransactionEvent({})
-    mockNewAgentDeployFunction.filterFunction = jest.fn()
+console.log(`handle tx: ${handleTransaction}`)
+
+describe("New Agent Deployment",  async () => {
+  describe('detect new agent deployments by Forta deployer account', async () => {
+    const mockDeployment = createTransactionEvent({})
+    mockDeployment.filterFunction = jest.fn()
 
     beforeEach(() => {
-      mockNewAgentDeployFunction.filterFunction.mockReset()
+      mockDeployment.filterFunction.mockReset()
     })
 
     it('returns empty findings if there are no new deployment', async () => {
-      mockNewAgentDeployFunction.filterFunction.mockReturnValue([])
-      const findings = await handleTransaction(mockNewAgentDeployFunction)
+      mockNewAgentDeployFunction.filterFunction.mockReturnValue([]);
+      
+      const findings = await handleTransaction(mockDeployment)
       console.log(`finding: ${findings}`)
-      // expect(findings).toStrictEqual([])
-      // expect(mockNewAgentDeployFunction.filterFunction).toHaveBeenCalled(1)
-      // expect()
+      expect(findings).toStrictEqual([])
+      expect(mockDeployment.filterFunction).toHaveBeenCalled(1)
     })
   })
 });
